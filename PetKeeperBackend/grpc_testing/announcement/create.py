f66f0a80b82310fcee629b90ec9b5568204e3488
LDTips <batruchs@gmail.com>
2025-01-12 15:59:16 +0100

    Added admin functionality for Announcement; tests update

--- PetKeeperBackend/grpc_testing/announcement/create.py
+++ PetKeeperBackend/grpc_testing/announcement/create.py
@@ -2,24 +2,58 @@ import grpc
 import user_pb2
 import user_pb2_grpc
 from auth.get import get_token
-# Create a channel to connect to the .NET server
+from user.create import users, get_user_ids
+from address.create import addresses, get_address_ids
+from animal.create import animals, get_animal_ids
+import os
+from datetime import datetime
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-email = "new_user22222@example.com"
-password = "securepassword123"
-channel = grpc.insecure_channel('localhost:8080')  # Update with your server's address if needed
-stub = user_pb2_grpc.AnnouncementServiceStub(channel)  # Replace Greeter with your service name
+user_ids = get_user_ids()
+animal_ids = get_animal_ids()
+primary_address_ids, secondary_address_ids = get_address_ids()
 
-create_request = user_pb2.AnnouncementCreate(
-    keeper_id='076277c2-aa04-4b62-8da5-f6e2cd78e753',
-    animal_id='040825f2-9d12-47f1-9c03-6b712c872917',
-    keeper_profit=1000,
-    is_negotiable=False,
-    description='Wyjeżdżam na wakację. Potrzebuję osoby do zajęcia się przemiłym fafikiem na 3 dni :D',
-    start_term=1735340464,
-    end_term=1735682464,
-    address_id='ac51df27-96b1-4284-8039-4849c80cf441'
-)
+channel = grpc.insecure_channel('localhost:8080')
+stub = user_pb2_grpc.AnnouncementServiceStub(channel)
 
-token = get_token(email, password, channel=channel)
-create_response = stub.CreateAnnouncement(create_request, metadata=[("authorization", f"Bearer {token}")])
-print("CreateAnnouncement Response:", create_response)
+announcements = [
+    {
+        'animal_id': animal_ids[3],  # Animal of user[1]
+        'keeper_profit': 400,
+        'is_negotiable': True,
+        'description': 'Grubiutki inżynier potrzebuje opieki na tydzień',
+        'start_term': int(datetime.strptime("2025-02-14 12:00:00", DATE_FORMAT).timestamp()),
+        'end_term': int(datetime.strptime("2025-02-21 15:00:00", DATE_FORMAT).timestamp()),
+        'address_id': primary_address_ids[1]
+    },
+    {
+        # 'author_id': user_ids[0],
+        'animal_id': animal_ids[2],  # Animal of user[2]
+        'keeper_profit': 100,
+        'is_negotiable': False,
+        'description': 'Szukam krótkoterminowej opieki nad moim kotkiem. Bardzo nie lubi być samemu w domu',
+        'start_term': int(datetime.strptime("2025-02-13 16:00:00", DATE_FORMAT).timestamp()),
+        'end_term': int(datetime.strptime("2025-02-13 23:30:00", DATE_FORMAT).timestamp()),
+        'address_id': primary_address_ids[2]
+    }
+]
+
+
+def get_announcement_ids():
+    with open(os.path.join(script_dir, 'announcement_ids.txt'), 'r') as f:
+        return [line.strip() for line in f.readlines()]
+
+
+if __name__ == "__main__":
+    ids = []
+    for usr_id, usr, announcement in zip(user_ids[1:], users[1:], announcements):
+        create_request = user_pb2.AnnouncementCreate(**announcement)
+
+        token = get_token(usr['email'], usr['password'], channel=channel)
+        create_response = stub.CreateAnnouncement(create_request, metadata=[("authorization", f"Bearer {token}")])
+        ids.append(create_response.id)
+        print("AnnouncementCreate Response:\n", create_response)
+
+    with open('announcement_ids.txt', 'w') as f:
+        f.writelines(i + '\n' for i in ids)
--- PetKeeperBackend/grpc_testing/announcement/update.py
+++ PetKeeperBackend/grpc_testing/announcement/update.py
@@ -1,20 +1,58 @@
 import grpc
 import user_pb2
 import user_pb2_grpc
+import sys
 from auth.get import get_token
-# Create a channel to connect to the .NET server
-channel = grpc.insecure_channel('localhost:8080')  # Update with your server's address if needed
-stub = user_pb2_grpc.UserServiceStub(channel)  # Replace Greeter with your service name
+from user.create import users, get_user_ids
+from address.create import addresses, get_address_ids
+from animal.create import animals, get_animal_ids
+from announcement.create import announcements, get_announcement_ids
+from datetime import datetime
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-email = "new_user22222@example.com"
-password = "securepassword123"
-# 1. get a new user
-id = user_pb2.UserIdentifier(email=email)
-update_request = user_pb2.UserUpdate(
-    user_id=id,
-    is_admin=True,
-    avatar_url="/example/path/to/file/on/machine.png"
-)
-token = get_token(email, password, channel=channel)
-update_response = stub.UpdateUser(update_request, metadata=[("authorization", f"Bearer {token}")])
-print("UpdateUser Response:\n", update_response)
+user_ids = get_user_ids()
+animal_ids = get_animal_ids()
+primary_address_ids, secondary_address_ids = get_address_ids()
+announcement_ids = get_announcement_ids()
+
+channel = grpc.insecure_channel('localhost:8080')
+stub = user_pb2_grpc.AnnouncementServiceStub(channel)
+
+update_requests = [
+    user_pb2.AnnouncementUpdate(
+        id=announcement_ids[0],  # Belongs to user[1]
+        keeper_profit=450,
+        is_negotiable=False,
+        address_id=secondary_address_ids[1]
+    ),
+    user_pb2.AnnouncementUpdate(  # Attempt updating other person's announcement (should fail)
+        id=announcement_ids[1],  # Belongs to user[2]
+        keeper_profit=200,
+        description='Hax04',
+        is_negotiable=True
+    ),
+    user_pb2.AnnouncementUpdate(  # Update other person's announcement as admin
+        id=announcement_ids[1],
+        end_term=int(datetime.strptime("2025-02-13 22:00:00", DATE_FORMAT).timestamp()),
+        keeper_profit=80,
+        is_negotiable=False  # Check if non-changing update is returned (should not be)
+    ),
+    # Attempt updating other person's announcement as admin, but set address not owned by this user (should fail)
+    user_pb2.AnnouncementUpdate(
+        id=announcement_ids[0],
+        keeper_profit=200,
+        description='Hax04',
+        is_negotiable=True,
+        address_id=primary_address_ids[2]
+    ),
+]
+
+announcement_users = [users[1], users[1], users[2], users[2]]
+announcement_user_ids = [user_ids[1], user_ids[1], user_ids[2], user_ids[2]]
+for user, user_id, update_request in zip(announcement_users, announcement_user_ids, update_requests):
+    token = get_token(user['email'], user['password'], channel=channel)
+    try:
+        update_response = stub.UpdateAnnouncement(update_request, metadata=[("authorization", f"Bearer {token}")])
+        print("AnnouncementUpdate Response:\n", update_response)
+    except grpc.RpcError as e:
+        print(f"Error: {e}", file=sys.stderr)
