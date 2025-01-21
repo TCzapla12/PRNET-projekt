0dab9c0616778000ab45d78ba5877ecbf98795ee
LDTips <batruchs@gmail.com>
2025-01-21 15:16:32 +0100

    Merge squash dev_admin_webapp into dev

---
+++ PetKeeperAdminWebapp/Services/AuthenticationStateProvider.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.WebUtilities;
+using Blazored.LocalStorage;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace MyBlazorApp.Services
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public CustomAuthenticationStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorage.GetItemAsync<string>("auth_token");
+
+            var identity = string.IsNullOrEmpty(token)
+                ? new ClaimsIdentity()
+                : new ClaimsIdentity(ParseClaimsFromJwt(token), "Bearer");
+
+            var user = new ClaimsPrincipal(identity);
+            var authState = new AuthenticationState(user);
+
+            return authState;
+        }
+
+        public void MarkUserAsAuthenticated(string token)
+        {
+            var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "Bearer");
+            var user = new ClaimsPrincipal(identity);
+
+            var authState = new AuthenticationState(user);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(authState));
+        }
+
+        public void MarkUserAsLoggedOut()
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity());
+            var authState = new AuthenticationState(user);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(authState));
+        }
+
+        private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = WebEncoders.Base64UrlDecode(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
+        }
+    }
+}
